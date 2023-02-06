3aa4bfe922f09b9d5e6757fd8c21c659bea02c0f
David Puscas <david.puscas93@gmail.com>
2023-02-06 22:23:59 +0200

    update initial-scale for meta viewport

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import '../styles/globals.css';
 
+import Head from 'next/head';
+
 import { StoreProvider } from '../store/store.context';
 
 export default function App({ Component, pageProps }) {
   return (
     <StoreProvider>
+      <Head>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+      </Head>
       <Component {...pageProps} />
     </StoreProvider>
   );
